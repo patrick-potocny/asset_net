478a455a1b4088e24936a833d9e8f440c568b10c
patrick-potocny <pato.potocny85@gmail.com>
2022-12-27 22:24:50 +0100

    Segmented Picker and media queries

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Header from './components/Header';
 import SegmentedPicker from './components/SegmentedPicker';
 function App() {
+
+  const [assetType, setAssetType] = useState('')
+
   return (
     <>
       <Header />
-      <SegmentedPicker />
+      <SegmentedPicker
+        name="main"
+        callback={(val) => setAssetType(val)}
+        controlRef={useRef()}
+        options={[
+          {
+            label: "All assets",
+            value: "all_assets",
+            ref: useRef()
+          },
+          {
+            label: "Crypto",
+            value: "crypto",
+            ref: useRef()
+          },
+          {
+            label: "Stocks",
+            value: "stocks",
+            ref: useRef()
+          }
+        ]}
+      />
+      <p>Selected {assetType}</p>
     </>
   );
 }
--- src/components/SegmentedPicker.js
+++ src/components/SegmentedPicker.js
@@ -1,11 +1,59 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import PropTypes from 'prop-types'
 
-function SegmentedPicker() {
+// reused component from https://letsbuildui.dev/articles/building-a-segmented-control-component
+function SegmentedPicker({ name, options, callback, controlRef,}) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const componentReady = useRef();
+
+  useEffect(() => {
+    componentReady.current = true;
+  }, []);
+
+  useEffect(() => {
+    const activeSegmentRef = options[activeIndex].ref;
+    const { offsetWidth, offsetLeft } = activeSegmentRef.current;
+    const { style } = controlRef.current;
+
+    style.setProperty("--highlight-width", `${offsetWidth}px`);
+    style.setProperty("--highlight-x-pos", `${offsetLeft}px`);
+  }, [activeIndex, callback, controlRef, options]);
+
+  const onInputChange = (value, index) => {
+    setActiveIndex(index);
+    callback(value, index);
+  };
 
   return (
-    <div className="segmented-control">
+    <div className="container controls-container" ref={controlRef}>
+      <div className={`controls ${componentReady.current ? "ready" : "idle"}`}>
+        {options?.map((item, i) => (
+          <div
+            key={item.value}
+            className={`segment ${i === activeIndex ? "active" : "inactive"}`}
+            ref={item.ref}
+          >
+            <input
+              type="radio"
+              value={item.value}
+              id={item.label}
+              name={name}
+              onChange={() => onInputChange(item.value, i)}
+              checked={i === activeIndex}
+            />
+            <label htmlFor={item.label}>{item.label}</label>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
+
 export default SegmentedPicker;
+SegmentedPicker.propTypes = {
+  name: PropTypes.string.isRequired,
+  options: PropTypes.array.isRequired,
+  callback: PropTypes.func.isRequired,
+  controlRef: PropTypes.object.isRequired,
+};
